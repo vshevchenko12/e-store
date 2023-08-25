144484554b9d49876b1683e17c34a8c3bdd7fee6
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-08-25 13:57:07 -0300

    add docker-compose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.active_record.sqlite3_production_warning = false
+
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
